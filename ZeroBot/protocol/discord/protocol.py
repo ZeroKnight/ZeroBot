26ca0d4f68363f418217b9403112484e283a0020
ZeroKnight <xzeroknightx@gmail.com>
2020-02-03 20:03:22 -0800

    Commit latest scribblings in protocol modules

--- ZeroBot/protocol/discord/protocol.py
+++ ZeroBot/protocol/discord/protocol.py
@@ -7,6 +7,9 @@ import asyncio
 
 import discord
 
+import ZeroBot.common.abc as abc
+from ZeroBot.protocol.context import Context
+
 def module_register(core):
     global CORE
     CORE = core
@@ -20,13 +23,37 @@ def module_unregister():
 def module_get_context(eventloop: asyncio.AbstractEventLoop):
     # TEMP: get this stuff from config later
     print(f'module_get_context: eventloop={eventloop}')
-    ctx = Context(loop=eventloop)
-    coro = ctx.start('token goes here')
+    ctx = DiscordContext(loop=eventloop)
+    coro = ctx.start('NjcyMjQxNTcyNDgxNTk3NDQw.XjIoUA.hhtREouFVWh-P9ibmYd0KNn_Glg')
     return (ctx, coro)
 
-class Context(discord.Client):
+class DiscordMessage(abc.Message):
+    def __init__(self, message: discord.Message):
+        self.source = message.author
+        self.destination = message.channel
+        self.content = message.content
+        self.time = message.created_at
+        self._original = message
+
+    @property
+    def original(self):
+        return self._original
+
+    def __eq__(self, other):
+        pass # TODO
+
+class DiscordContext(Context, discord.Client):
+    """blah
+    """
+
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
+        msg = DiscordMessage(message)
+        await CORE.module_send_event('message', self, msg)
+
+    async def module_message(self, destination, message):
+        await destination.send(message)
+
--- ZeroBot/protocol/irc/protocol.py
+++ ZeroBot/protocol/irc/protocol.py
@@ -7,6 +7,10 @@ import asyncio
 
 import pydle
 
+import ZeroBot.common.abc as abc
+from ZeroBot.protocol.context import Context
+from .classes import IRCChannel, IRCMessage, IRCServer, IRCUser
+
 def module_register(core):
     global CORE
     CORE = core
@@ -20,23 +24,38 @@ def module_unregister():
 def module_get_context(eventloop: asyncio.AbstractEventLoop):
     # TEMP: get this stuff from config later
     print(f'module_get_context: eventloop={eventloop}')
-    ctx = Context('ZeroBot', eventloop=eventloop)
+    ctx = IRCContext('ZeroBot', eventloop=eventloop)
     coro = ctx.connect('wazu.info.tm')
     return (ctx, coro)
 
-# TBD: Include a ZeroBot-API level ABC in inheritance?
-class Context(pydle.Client):
+class IRCContext(Context, pydle.Client):
     """blah
     """
 
     async def on_connect(self):
         await super().on_connect()
-        print('!! connected')
+
+        # TODO: call something like Core.module_on_connect
+
+        print('[irc] connected to blahblah')
         await self.join('#zerobot')
 
     async def on_join(self, channel, user):
         await super().on_join(channel, user)
-        if user == self.nickname:
-            await self.message(channel, 'Hello, world!')
-        else:
-            await self.message(channel, f'Hi there, {user}!')
+
+        channel = IRCChannel(channel)
+        # TODO: pydle only passes the nickname, so we need pull the other values
+        # from the server (cache this somewhere?)
+        user = IRCUser(user, '', '', '', False)
+        await CORE.module_send_event('join', self, channel, user)
+
+    async def on_message(self, destination: str, source: str, message: str):
+        await super().on_message(destination, source, message)
+
+        # TODO: time attribute, ircv3 tags
+        msg = IRCMessage(source, destination, message, None, {})
+        await CORE.module_send_event('message', self, msg)
+
+    async def module_message(self, destination, message):
+        await self.message(destination, message)
+
