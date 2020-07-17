38b5ae78be83b1630fd5e7ceac893c8f647201b2
ZeroKnight <xzeroknightx@gmail.com>
2020-07-17 03:28:08 -0700

    Add `is_reload` helper to `ModuleCmdStatus` enum

--- ZeroBot/common/enums.py
+++ ZeroBot/common/enums.py
@@ -43,3 +43,8 @@ class ModuleCmdStatus(Enum):
     def is_ok(cls, status: 'ModuleCmdStatus') -> bool:
         """Return whether the given status is an "OK" type."""
         return status is cls.LOAD_OK or status is cls.RELOAD_OK
+
+    @classmethod
+    def is_reload(cls, status: 'ModuleCmdStatus') -> bool:
+        """Return whether the given status is a "RELOAD" type."""
+        return status in (cls.RELOAD_OK, cls.RELOAD_FAIL, cls.NOT_YET_LOADED)
