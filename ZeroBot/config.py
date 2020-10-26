d7ff6324a154081735b57248f0c919e783c9a5b4
ZeroKnight <xzeroknightx@gmail.com>
2020-10-26 16:39:32 -0700

    Add a `map_reduce` utility function
    Refactored the use of `functools.reduce` in `Config.reset` into
    a utility function.

--- ZeroBot/config.py
+++ ZeroBot/config.py
@@ -7,12 +7,11 @@ from collections import ChainMap, UserDict
 from pathlib import Path
 from string import Template
 from typing import Any, Iterable, Mapping, Optional, Union
-from functools import reduce
-import operator
 
 import toml
 
 import ZeroBot
+from ZeroBot.util import map_reduce
 
 _configvars = {
     'botversion': ZeroBot.__version__
@@ -171,21 +170,41 @@ class Config(ConfigDict):
         toml.dump(self.data, new_path or self.path)
         self._last_state = self.data
 
+    def set(self, key_path: str, value: Any):
+        """Set a config key to a new value.
+
+        Parameters
+        ----------
+        key_path : str
+            The key to set. The key is interpreted as a dot-separated
+            identifier for a key under an arbitrarily deep hierarchy, e.g.
+            ``Core.Database.Filename``.
+        value : Any
+            The new value for the key.
+        """
+        if key_path is not None:
+            nodes = key_path.split('.')
+            key = nodes.pop()
+            target = map_reduce(nodes, self.data)
+            target[key] = value
+        else:
+            ValueError('key must not be None')
+
     def reset(self, key: str = None):
-        """Reset this config to its last loaded/saved state.
+        """Reset this config or a single key to its last loaded/saved state.
 
         Parameters
         ----------
         key : str, optional
             If specified, resets a single key instead of the whole config. The
-            value is interpreted as a dot-separated identifier for a key under
-            an arbitrarily deep hierarchy, e.g. ``Core.Database.Filename``.
+            key is interpreted as a dot-separated identifier for a key under an
+            arbitrarily deep hierarchy, e.g. ``Core.Database.Filename``.
         """
         if key is not None:
             nodes = key.split('.')
-            last = reduce(operator.getitem, nodes, self._last_state)
+            last = map_reduce(nodes, self._last_state)
             key = nodes.pop()
-            current = reduce(operator.getitem, nodes, self.data)
+            current = map_reduce(nodes, self.data)
             current[key] = last
         else:
             self.data = self._last_state
--- ZeroBot/util.py
+++ ZeroBot/util.py
@@ -4,7 +4,9 @@ Various internal utilities for ZeroBot. If you're working on a feature module,
 you *probably* want `ZeroBot.common` instead.
 """
 
-from typing import Any, Iterable
+import operator
+from functools import reduce
+from typing import Any, Iterable, List, Mapping, Union
 
 
 def gen_repr(obj: Any, attrs: Iterable, **kwargs) -> str:
@@ -34,3 +36,34 @@ def gen_repr(obj: Any, attrs: Iterable, **kwargs) -> str:
         extras = ' '.join(f'{attr}={val!r}' for attr, val in kwargs.items())
         body += ' ' + extras
     return f'<{name} {body}>'
+
+
+def map_reduce(key_path: Union[str, List[str]],
+               mapping: Mapping[str, Any]) -> Any:
+    """Reduce a mapping, returning a value from an arbitrarily deep hierarcy.
+
+    The result of calling this function is the same as successively
+    subscripting each key in the `key_path`, starting from `mapping`.
+
+    Parameters
+    ----------
+    key_path : str or List[str]
+        A collection of sequential child keys into `mapping`. May be given as
+        either a list of strings or a single string with dots (``.``)
+        delimiting keys.
+    mapping : Mapping[str, Any]
+        The mapping to subscript.
+
+    Example
+    -------
+    The following lines are equivalent:
+
+        map_reduce('foo.bar.baz', things)
+        # Is the same as:
+        map_reduce(['foo', 'bar', 'baz'], things)
+        # Is the same as:
+        things['foo']['bar']['baz']
+    """
+    if isinstance(key_path, str):
+        key_path = key_path.split('.')
+    return reduce(operator.getitem, key_path, mapping)
