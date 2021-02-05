890e09dc90d1ea4af302b5d6d1eecbe74d4386d0
ZeroKnight <xzeroknightx@gmail.com>
2021-02-05 04:09:59 -0800

    Ride the walrus

--- ZeroBot/feature/quote.py
+++ ZeroBot/feature/quote.py
@@ -1040,15 +1040,13 @@ async def quote_quick(ctx, parsed):
     """Shortcuts for adding a quote to the database."""
     lines = []
     cached = False
-    user = parsed.args['user']
-    if user is not None:
+    if user := parsed.args['user'] is not None:
         user = user.lstrip('@')
     submitter = await get_participant(
         parsed.args['submitter'] or parsed.invoker.name)
     style = getattr(QuoteStyle, parsed.args['style'].title())
     if parsed.args['date']:
-        date = read_datestamp(parsed.args['date'])
-        if date is None:
+        if date := read_datestamp(parsed.args['date']) is None:
             await CORE.module_send_event('invalid_command', ctx, parsed.msg)
             return
     else:
