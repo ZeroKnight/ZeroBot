ZeroKnight/ZeroBot

commit fdb5c7fc399132d9dc997e2211d543271a831da7

Add `clean_content` to `abc.Message`
Lifted from Discord's implementation, `clean_content` returns the
content as it would be presented to a client.

Commands will be processed on the clean content version to avoid, for
example, Discord mention sequences from showing up in command arguments.

ZeroBot/core.py
@@ -1107,7 +1107,7 @@ class Core:
 
         invoker = cmd_msg.source
         dest = cmd_msg.destination
-        cmd_str = cmd_msg.content
+        cmd_str = cmd_msg.clean_content
         if not cmd_str.startswith(self.cmdprefix):
             raise NotACommand(f'Not a command string: {cmd_str}')
         try:

ZeroBot/common/abc.py
@@ -141,6 +141,15 @@ class Message(ProtocolDetails, metaclass=ABCMeta):
     def strip_action_str(string: str) -> str:
         """Strip the action formatting from the given string."""
 
+    @property
+    @abstractmethod
+    def clean_content(self) -> str:
+        """Return a "clean" version of the message content.
+
+        Specifically, mentions are converted into the proper name they
+        represent. For example, in Discord, `<@{id}>` becomes `username`.
+        """
+
     def is_action(self) -> bool:
         """Check whether the message is an action."""
         return self.is_action_str(self.content)

ZeroBot/protocol/discord/classes.py
@@ -126,7 +126,7 @@ class DiscordChannel(zabc.Channel, discord.TextChannel):
         return self._original
 
 
-class DiscordMessage(zabc.Message, discord.Message):
+class DiscordMessage(discord.Message, zabc.Message):
     """Represents a Discord message of any type."""
 
     def __init__(self, message: discord.Message):

commit b1244207d37b4482aded93821cdb2c785cda8d2f

Fix regression in `wait` command
Introduced in fdb5c7fc.

ZeroBot/core.py
@@ -1499,8 +1499,10 @@ class Core:
             delay = float(delay.replace(suffix, '')) * factor[suffix]
         cmd = parsed.args['command']
         args = ' '.join(parsed.args['args']) or None
+        # XXX: Should have a proper way to do this in abc.Message
         parsed.msg.content = f'{self.cmdprefix}{cmd}'
         parsed.msg.content += f' {args}' if args else ''
+        parsed.msg.clean_content = parsed.msg.content
         await self.module_commanded(parsed.msg, ctx, delay)
 
     async def module_command_cancel(self, ctx, parsed):